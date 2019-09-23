740e61919e2f6207f993aed1fff93f58b38e2bdc
Paras Garg <parasgarg.github@gmail.com>
2019-09-24 01:15:19 +0530

    Assignment 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Main from './components/MainComponent';
-import './App.css';
+import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { ConfigureStore } from './redux/configureStore';
+import { ConfigureStore } from "./redux/configureStore";
+import Main from "./components/MainComponent";
+import "./App.css";
 
 const store = ConfigureStore();
 
-function App() {  
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div>
-          <Main />
-        </div>
-      </BrowserRouter>
-    </Provider>    
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
-
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
+} from "reactstrap";
 import { Link } from "react-router-dom";
-import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderMenuItem({ dish, onClick }) {
   return (
@@ -16,59 +23,50 @@ function RenderMenuItem({ dish, onClick }) {
     </Card>
   );
 }
-
-const Menu = (props) => {
-  const menu = props.dishes.dishes.map((dish) => {
+const Menu = props => {
+  const menu = props.dishes.dishes.map(dish => {
     return (
-      <div key={dish.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem dish={dish} />
+      <div className="col-12 col-md-5 m-1" key={dish.id}>
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
   });
-
   if (props.dishes.isLoading) {
     return (
-        <div className="container">
-          <div className="row">
-            <Loading />
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h4>{props.dishes.errMess}</h4>
           </div>
         </div>
-      )
-  }
-  else if (props.dishes.errMess) {
-      return (
-          <div className="container">
-              <div className="row">
-                  <h4>{props.dishes.errMess}</h4>
-              </div>
-          </div>
-      )
-  }
-  else {
+      </div>
+    );
+  } else
     return (
       <div className="container">
         <div className="row">
           <Breadcrumb>
             <BreadcrumbItem>
-              <Link to='/home'>Home</Link>
+              <Link to="/home">Home</Link>
             </BreadcrumbItem>
-            <BreadcrumbItem active>
-              <Link>Menu</Link>
-            </BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
           </Breadcrumb>
           <div className="col-12">
             <h3>Menu</h3>
             <hr />
           </div>
         </div>
-        <div className="row">
-          { menu }
-        </div>
+        <div className="row">{menu}</div>
       </div>
-      );
-  }
-}
-
-
+    );
+};
 
 export default Menu;
